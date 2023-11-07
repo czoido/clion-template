abea4a3bb9a0bd95f5f538e6cfd89fbebaa0acc7
GitHub Action <action@github.com>
2023-11-07 13:05:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/czoido/cliontemplate/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/czoido/cliontemplate/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.czoido.cliontemplate.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.czoido.cliontemplate.MyBundle
+import com.github.czoido.cliontemplate.services.MyProjectService
 import javax.swing.JButton
 
 
